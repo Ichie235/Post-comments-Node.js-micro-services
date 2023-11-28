7b869f38a5efcaaba08533e56cc525f018b818d3
Ichie Chinemerem Richard <108372995+Ichie235@users.noreply.github.com>
2023-11-28 11:38:57 +0100

    add listPost.jsx to show user posts

--- client/posts-comment-app/src/App.jsx
+++ client/posts-comment-app/src/App.jsx
@@ -1,9 +1,14 @@
 import CreatePost from "./CreatePost";
+import ListPost from "./ListPost";
 
 function App() {
   return (
     <div className="container">
+      <h1>Creat Posts</h1>
       <CreatePost />
+      <hr />
+      <h1>Posts</h1>
+      <ListPost />
     </div>
   );
 }
